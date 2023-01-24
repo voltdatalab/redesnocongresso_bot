ccab77e4bd9b894446b9722a5160296804db22c3
JMallone <michel@voltdata.info>
2023-01-23 23:16:23 -0300

    renomeando os arquivos em vez de apagar, pode ser util para futuras analises

--- crontab.py
+++ crontab.py
@@ -3,13 +3,16 @@
 import nocongresso
 import enviar_tweets
 import os
+import hashlib
 
 if __name__ == "__main__":
     nocongresso.main()
     enviar_tweets.main()
 
     try:
-        os.remove('dados/tweets.json')
+        # renomear arquivo de tweets
+        hash_file = hashlib.md5(open('dados/tweets.json', 'rb').read()).hexdigest()
+        os.rename('dados/tweets.json', 'dados/tweets-' + hash_file + '.json')
     except Exception as e:
         print(e)
         
