23c05203468ac1aa6652149f60bf61c83eb185d4
JMallone <michel@voltdata.info>
2023-01-31 14:37:23 -0300

    Typo

--- enviar_tweets.py
+++ enviar_tweets.py
@@ -84,8 +84,8 @@ async def main():
                 # response = api.update_status_with_media(status=normalize_tweets.norm(text), filename='html/out.png')
 
                 res = api.media_upload("html/out.png")
-                text_no_emoji = normalize_tweets.removeNone(text_no_emoji)
-                text_no_emoji = normalize_tweets.removeEmoji(text)
+                text_no_emoji = normalize_tweets.removeNone(text)
+                text_no_emoji = normalize_tweets.removeEmoji(text_no_emoji)
                 api.create_media_metadata(res.media_id, alt_text=titulo+'\n\n'+text_no_emoji)
                 status = api.update_status(media_ids = [res.media_id], status=normalize_tweets.norm(text))
              
